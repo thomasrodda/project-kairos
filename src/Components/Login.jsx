ab3b711ca4d296b89309c391241c8675bb5f5cc1
thomasrodda <thomasmichaelrodda@gmail.com>
2023-09-29 12:19:05 +0100

    Google Login Successful, login page styled.

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { auth } from '../firebase';
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import '../index.css';
 
 const signInWithGoogle = () => {
   const provider = new GoogleAuthProvider();
@@ -9,9 +10,23 @@ const signInWithGoogle = () => {
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <div className="mainMenu menuBackground">
+        <div className="menuContentArea boxShadowBlackL flex flex-col items-center justify-center">
+            <div className="workspaceCreationHeading">
+                <h1 className="text-pageName font-bold">Sign in to Slate-AI</h1>
+                {/* The text changes based on the stage */}
+                <h2 className="text-h4 text-grey mx-2">Log in with your Google account</h2>
+            </div>
+            <div id="signInGoogle" className="flex justify-center">
+                <button onClick={signInWithGoogle} type="submit" className="w-full z-10">
+                    <div className="SpecialButton SpecialButton:hover gradient-border gradient-border:after">
+                        <h2 className="SpecialButtonText">
+                          Sign in with Google
+                        </h2>
+                    </div>
+                </button>
+            </div>
+        </div>
     </div>
   );
 };
